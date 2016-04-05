dd44438edd4c2fb2966b19414310168188e22cdd
Lucinda-K <lkrahl@nd.edu>
2016-04-04 20:28:52 -0400

    Barcode scanner works, displays data and type

--- PocketLibrary/PocketLibrary/ScannerViewController.swift
+++ PocketLibrary/PocketLibrary/ScannerViewController.swift
@@ -11,6 +11,111 @@ import AVFoundation
 
 class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
 
+    @IBOutlet weak var cameraView: UIImageView!
+    
+    @IBOutlet weak var dataLabel: UILabel!
+    
+    @IBOutlet weak var dataTypeLabel: UILabel!
+    
+    //MARK: Properties
+    /// Runs the capture session.
+    let captureSession = AVCaptureSession()
+    /// The device used as input for the capture session.
+    var captureDevice:AVCaptureDevice?
+    /// The UI layer to display the feed from the input source, in our case, the camera.
+    var captureLayer:AVCaptureVideoPreviewLayer?
+    
+    //MARK: View lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        self.setupCaptureSession()
+    }
+    
+    private func setupCaptureSession(){
+        self.captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        let deviceInput:AVCaptureDeviceInput
+        do {
+            deviceInput = try AVCaptureDeviceInput(device: captureDevice)
+        } catch {
+            return
+        }
+        
+        if (captureSession.canAddInput(deviceInput)){
+            // Show live feed
+            captureSession.addInput(deviceInput)
+            self.setupPreviewLayer({
+                self.captureSession.startRunning()
+                self.addMetaDataCaptureOutToSession()
+            })
+        } else {
+            self.showError("Error while setting up input captureSession.")
+        }
+    }
+    
+    /**
+     Handles setting up the UI to show the camera feed.
+     
+     - parameter completion: Completion handler to invoke if setting up the feed was successful.
+     */
+    private func setupPreviewLayer(completion:() -> ())
+    {
+        self.captureLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        if let capLayer = self.captureLayer {
+            capLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+            capLayer.frame = self.cameraView.frame
+            self.view.layer.addSublayer(capLayer)
+            completion()
+        } else {
+            self.showError("An error occured beginning video capture")
+        }
+    }
+    
+    //MARK: Metadata capture
+    /**
+    Handles identifying what kind of data output we want from the session, in our case, metadata and the available types of metadata.
+    */
+    private func addMetaDataCaptureOutToSession()
+    {
+        let metadata = AVCaptureMetadataOutput()
+        self.captureSession.addOutput(metadata)
+        metadata.metadataObjectTypes = metadata.availableMetadataObjectTypes
+        metadata.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+    }
+    
+    //MARK: Delegate Methods
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!)
+    {
+        for metadata in metadataObjects{
+            let decodedData:AVMetadataMachineReadableCodeObject = metadata as! AVMetadataMachineReadableCodeObject
+            self.dataLabel.text = decodedData.stringValue
+            self.dataTypeLabel.text = decodedData.type
+        }
+    }
+    
+    //MARK: Utility Functions
+    /**
+    Shows any error that may occur via an alert view.
+    
+    - parameter error: The error message.
+    */
+    private func showError(error:String)
+    {
+        let alertController = UIAlertController(title: "Error", message: error, preferredStyle: .Alert)
+        let dismiss:UIAlertAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Destructive, handler:{(alert:UIAlertAction) in
+            alertController.dismissViewControllerAnimated(true, completion: nil)
+        })
+        alertController.addAction(dismiss)
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    
+    
+    /*
     // https://www.hackingwithswift.com/example-code/media/how-to-scan-a-barcode
     
     var captureSession: AVCaptureSession!
@@ -113,7 +218,7 @@ class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDel
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    */
 
 }
 
--- PocketLibrary/PocketLibrary/SecondViewController.swift
+++ PocketLibrary/PocketLibrary/SecondViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class SecondViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
